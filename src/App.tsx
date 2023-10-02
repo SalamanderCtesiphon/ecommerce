c904c96867b146561e448b6d82cec786d90928cf
salamanderctesiphon <swbrookshire@gmail.com>
2023-10-02 17:07:57 -0500

    created routes and pages for the store, success and cancel pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
 import './App.css'
 import Navbar from './components/Navbar'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { Container } from 'react-bootstrap'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Store from './pages/Store'
+import Success from './pages/Success'
+import Cancel from './pages/Cancel'
 
 function App() {
 
   return (
     <>
-      <Navbar />
-      <h1 className="text-3xl font-bold">
-        Hello world!
-      </h1>
-
+      <Container>
+        <Navbar />
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<Store />} />
+            <Route path='success' element={<Success />} />
+            <Route path='cancel' element={<Cancel />} />
+          </Routes>
+        </BrowserRouter>
+      </Container>
     </>
   )
 }
