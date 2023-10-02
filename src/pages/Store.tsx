e81bdae0a103161b6c4aa2383d3cc59ed2cc4259
salamanderctesiphon <swbrookshire@gmail.com>
2023-10-02 17:36:06 -0500

    fixed a type error with a bootstap rule

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,16 +1,14 @@
 import { Row, Col } from 'react-bootstrap'
+import { productsArray } from '../productStore'
 
 function Store() {
   return (
     <>
-      <h1>Welcome to the store!</h1>
+      <h1 className='p-3 text-center'>Welcome to the store!</h1>
       <Row xs={1} md={3} className='g-4'>
-        <Col align="center">
-          <h1>Product</h1>
-        </Col>
-        <Col align="center">
-          <h1>Product</h1>
-        </Col>
+        {productsArray.map((product, idx) => (
+          <h1>{product.title}</h1>
+        ))}
       </Row>
     </>
   )
