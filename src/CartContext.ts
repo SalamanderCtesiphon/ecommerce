a4c0141060467ea03cf9088495bb25ac1152aa4c
salamanderctesiphon <swbrookshire@gmail.com>
2023-10-02 19:55:33 -0500

    working on the cart context

--- src/CartContext.ts
+++ src/CartContext.ts
@@ -1,8 +1,78 @@
 import { createContext, useState } from "react";
 import { productsArray } from "./productStore";
 
-const CartContext = createContext({
+export const CartContext = createContext({
   items: [],
   getProductQuantity: () => {},
-  
+  addOnetoCart: () => {},
+  removeOneFromCart: () => {},
+  deleteFromCart: () => {},
+  getTotalCost: () => {},
 })
+
+export function CartProvider({children}) {
+  const [cartProducts, setCartProducts] = useState([])
+
+  function getProductQuantity(id: any) {
+    cartProducts.find(product => product.id === id)?.quantity
+
+    if(quantity === undefined) {
+      return 0
+    }
+
+    return quantity
+  }
+
+  function addOnetoCart(id: any) {
+    const quantity = getProductQuantity(id)
+
+    if(quantity === 0) {
+      setCartProducts([
+        ...cartProducts,
+        {
+          id: id,
+          quantity: 1
+        }
+      ])
+    } else {
+      setCartProducts(
+        cartProducts.map(
+         product => 
+         product.id === id
+         ? { ...product, quantity: product.quantity + 1 }
+         : 
+         product 
+        )
+      )
+    }
+  }
+
+  function removeOneFromCart() {
+
+  }
+
+  function deleteFromCart() {
+
+  }
+
+  function getTotalCost() {
+
+  }
+
+  const contextValue = {
+    items: [],
+    getProductQuantity,
+    addOnetoCart,
+    removeOneFromCart,
+    deleteFromCart,
+    getTotalCost,
+  }
+
+
+
+  return (
+    <CartContext.Provider value={contextValue}>
+      {chilren}
+    </CartContext.Provider>
+  )
+}
